f1bc77cc281da16006d6b322f4c0155ef6a6d15e
rishabhv1509 <rishabhv1509@gmail.com>
2019-02-17 12:50:31 +0530

    second

---
+++ server.js
@@ -0,0 +1,10 @@
+cont express=require('express');
+const app= express();
+const path=require.('path');
+
+app.use(express.static(__dirname+'/dist'));
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(res,req){
+	res.sendFile(path.join(__dirname+'/dist/index.html'));
+})
